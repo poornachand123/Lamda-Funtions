303923334c7751942b08dfd35d0d6a6d289e0e53
poornachand123 <44722287+poornachand123@users.noreply.github.com>
2018-11-14 04:04:25 -0800

    Add files via upload

---
+++ Demo9.py
@@ -0,0 +1,6 @@
+#reduse function in lambda
+from functools import reduce
+l1 = [10,20,30,40,50]
+print(reduce((lambda x,y :x+y),l1))
+print(reduce((lambda x,y :x*y),l1))
+print(reduce((lambda x,y :x//y),l1))
